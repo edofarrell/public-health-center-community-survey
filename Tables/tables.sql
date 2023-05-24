082f33a48a1247c757ada9d49ad71fbd0dd3588f
edofarrell <edo.farrell17@gmail.com>
2023-05-24 17:43:50 +0700

    add sql for create table

---
+++ Tables/tables.sql
@@ -0,0 +1,77 @@
+CREATE TABLE [dbo].[Role]
+(
+	[idRole] [INT] PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	[namaRole] [VARCHAR](50) NOT NULL,
+)
+
+CREATE TABLE [dbo].[User]
+(
+	[idUser] [INT] PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	[username] [VARCHAR](50) NOT NULL,
+	[password] [VARCHAR](50) NOT NULL,
+	[idRole] [INT] NOT NULL --FK Role(idRole)
+)
+
+CREATE TABLE [dbo].[Survei]
+(
+	[idSurvei] [INT] PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	[namaSurvei] [VARCHAR](100) NOT NULL,
+	[timestamp] [DATETIME] NOT NULL,
+	[tombstone] [BIT] NOT NULL
+)
+
+CREATE TABLE [dbo].[MengaksesSurvei]
+(
+	[idUser] [INT] NOT NULL, --FK User(idUser)
+	[idSurvei] [INT] NOT NULL, --FK Survei(idSurvei)
+	CONSTRAINT UC_AksesSurvei UNIQUE (idUser, idSurvei)
+)
+
+CREATE TABLE [dbo].[GroupJawaban]
+(
+	[idGroupJawaban] [INT] PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	[timestamp] [DATETIME] NOT NULL,
+	[idSurvei] [INT] NOT NULL --FK [Survei]([idSurvei])
+)
+
+CREATE TABLE [dbo].[PertanyaanSurvei]
+(
+	[idPertanyaanSurvei] [INT] PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	[pertanyaan] [VARCHAR](100) NOT NULL,
+	[timestamp] [DATETIME] NOT NULL,
+	[tombstone] [BIT] NOT NULL,
+	[idSurvei] [INT] NOT NULL --FK [Survei]([idSurvei])
+)
+
+CREATE TABLE [dbo].[JawabanDate]
+(
+	[idJawabanDate] [INT] PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	[jawabanDate] [DATETIME] NOT NULL,
+	[timestamp] [DATETIME] NOT NULL,
+	[tombstone] [BIT] NOT NULL,
+	[idPertanyaan] [INT] NOT NULL, --FK PertanyaanSurvei([idPertanyaanSurvei])
+	[idGroupJawaban] [INT] NOT NULL, --FK GroupJawaban([idGroupJawaban])
+	[idUser] [INT] NOT NULL --FK User([idUser])
+)
+
+CREATE TABLE [dbo].[JawabanNumeric]
+(
+	[idJawabanNumeric] [INT] PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	[jawabanNumeric] [FLOAT] NOT NULL,
+	[timestamp] [DATETIME] NOT NULL,
+	[tombstone] [BIT] NOT NULL,
+	[idPertanyaan] [INT] NOT NULL, --FK PertanyaanSurvei([idPertanyaanSurvei])
+	[idGroupJawaban] [INT] NOT NULL, --FK GroupJawaban([idGroupJawaban])
+	[idUser] [INT] NOT NULL --FK User([idUser])
+)	
+
+CREATE TABLE [dbo].[JawabanString]
+(
+	[idJawabanString] [INT] PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	[jawabanString] [VARCHAR](50) NOT NULL,
+	[timestamp] [DATETIME] NOT NULL,
+	[tombstone] [BIT] NOT NULL,
+	[idPertanyaan] [INT] NOT NULL, --FK PertanyaanSurvei([idPertanyaanSurvei])
+	[idGroupJawaban] [INT] NOT NULL, --FK GroupJawaban([idGroupJawaban])
+	[idUser] [INT] NOT NULL --FK User([idUser])
+)
