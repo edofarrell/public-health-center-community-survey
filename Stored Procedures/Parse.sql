a0a9e4778919a6ac49d9347fa6253959fb0114cc
edofarrell <edo.farrell17@gmail.com>
2023-05-28 13:48:50 +0700

    add function parse.sql

---
+++ Stored Procedures/Parse.sql
@@ -0,0 +1,49 @@
+/* 
+	String Format: "key1:value1,key2:value2,...,keyn:valuen"
+	First seperator: ','
+	Second seperator: ':'
+*/
+
+ALTER FUNCTION [Parse] 
+(
+	@str [VARCHAR](1000)
+)
+RETURNS @result TABLE
+(
+	[key] [VARCHAR](150),
+	[value] [VARCHAR](150)
+)
+AS
+BEGIN
+	DECLARE cursorRow CURSOR
+	FOR
+		SELECT 
+			value
+		FROM
+			STRING_SPLIT(@str, ',')
+	OPEN cursorRow
+
+	DECLARE @currRow varchar(150)
+	FETCH NEXT FROM
+		cursorRow
+	INTO
+		@currRow
+
+	WHILE(@@FETCH_STATUS=0)
+	BEGIN
+		INSERT INTO @result
+		SELECT
+			SUBSTRING(@currRow, 0, CHARINDEX(':', @currRow)),
+			SUBSTRING(@currRow, CHARINDEX(':', @currRow)+1, LEN(@currRow))
+
+		FETCH NEXT FROM
+			cursorRow
+		INTO
+			@currRow
+	END
+	
+	CLOSE cursorRow
+	DEALLOCATE cursorRow
+
+	RETURN
+END
