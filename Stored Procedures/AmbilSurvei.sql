a09eb067d7b338fa0422109fde5f39966a5b29d0
edofarrell <edo.farrell17@gmail.com>
2023-05-29 08:07:19 +0700

    add sp AmbilSurvei.sql

---
+++ Stored Procedures/AmbilSurvei.sql
@@ -0,0 +1,13 @@
+ALTER PROCEDURE [AmbilSurvei]
+	@idUser [INT]
+AS
+	SELECT
+		[Survei].[idSurvei],
+		[Survei].[namaSurvei]
+	FROM
+		[Survei]
+		INNER JOIN [MengaksesSurvei]
+			ON [Survei].[idSurvei] = [MengaksesSurvei].[idSurvei]
+	WHERE
+		[MengaksesSurvei].[idUser] = @idUser
+		AND	[Survei].[tombstone] = 1
