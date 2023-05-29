30fa593262d824b459ffd94a9db09b0f58f2701a
edofarrell <edo.farrell17@gmail.com>
2023-05-29 08:00:44 +0700

    add sp AmbilPertanyaan.sql

---
+++ Stored Procedures/AmbilPertanyaan.sql
@@ -0,0 +1,11 @@
+ALTER PROCEDURE [AmbilPertanyaan]
+	@idSurvei [INT]
+AS
+	SELECT
+		[idPertanyaanSurvei],
+		[pertanyaan]
+	FROM
+		[PertanyaanSurvei]
+	WHERE
+		[idSurvei] = @idSurvei
+		AND	[tombstone] = 1
