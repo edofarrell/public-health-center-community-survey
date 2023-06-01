768d6ecbe841a0bbad866655d5c978481cd6d84e
edofarrell <edo.farrell17@gmail.com>
2023-06-01 22:31:18 +0700

    change sp name EditUser.sql to UpdateUser.sql

---
+++ Stored Procedures/UpdateUser.sql
@@ -0,0 +1,12 @@
+ALTER PROCEDURE [EditUser]
+	@newUsername [VARCHAR](50),
+	@newPassword [VARCHAR](50),
+	@idUser [INT]
+AS
+	UPDATE 
+		[User]
+	SET
+		[username] = @newUsername,
+		[password] = @newPassword
+	WHERE
+		[idUser] = @idUser
