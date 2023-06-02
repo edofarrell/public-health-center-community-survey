4f2ad483ea15dfd15c4ecf6af9439693b250fd7e
edofarrell <edo.farrell17@gmail.com>
2023-06-02 10:22:25 +0700

    change function using openjson ParsePertanyaan.sql

--- Functions/ParsePertanyaan.sql
+++ Functions/ParsePertanyaan.sql
@@ -1,10 +1,86 @@
 /* 
+	Input Format (JSON Array):
+	[
+		{
+			"idPertanyaan": [NUMBER],
+			"pertanyaan":	[STRING],
+			"tipeJawaban":	[STRING]
+		},
+		{
+			"idPertanyaan": [NUMBER],
+			"pertanyaan": 	[STRING],
+			"tipeJawaban":	[STRING]
+		}
+	]
+
+	Output Format:
+	key				value
+	idPertanyaan	[INT]
+	pertanyaan		[VARCHAR](150)
+	tipeJawaban		[VARCHAR](10)
+*/
+ALTER FUNCTION [ParsePertanyaan] 
+(
+	@str [NVARCHAR](1000)
+)
+RETURNS @result TABLE
+(
+	[idPertanyaan] [INT],
+	[pertanyaan] [VARCHAR](150),
+	[tipeJawaban] [VARCHAR](10)
+)
+AS
+BEGIN
+	DECLARE
+		@currPertanyaan [VARCHAR](200)
+
+	DECLARE cursorPertanyaan CURSOR
+	FOR
+		SELECT
+			[value]
+		FROM
+			OPENJSON(@str)
+	OPEN cursorPertanyaan
+
+	FETCH NEXT FROM
+		cursorPertanyaan
+	INTO
+		@currPertanyaan
+
+	WHILE(@@FETCH_STATUS = 0)
+	BEGIN
+		INSERT INTO @result
+		SELECT
+			[idPertanyaan],
+			[pertanyaan],
+			[tipeJawaban]
+		FROM
+			OPENJSON(@currPertanyaan)
+		WITH
+			(
+				[idPertanyaan] [INT] '$.idPertanyaan',
+				[pertanyaan] [VARCHAR](150) '$.pertanyaan',
+				[tipeJawaban] [VARCHAR](11) '$.tipeJawaban'
+			)
+
+		FETCH NEXT FROM
+			cursorPertanyaan
+		INTO
+			@currPertanyaan
+	END
+
+	CLOSE cursorPertanyaan
+	DEALLOCATE cursorPertanyaan
+
+	RETURN
+END
+
+/*
 	String Format: "id1:pertanyaan1:tipe1,id2:pertanyaan2:tipe2,...,idn:pertanyaann:tipen"
 	First seperator: ','
 	Second seperator: ':'
 */
-
-ALTER FUNCTION [ParsePertanyaan] 
+/*ALTER FUNCTION [ParsePertanyaan] 
 (
 	@str [VARCHAR](1000)
 )
@@ -79,4 +155,4 @@ BEGIN
 	DEALLOCATE cursorRow
 
 	RETURN
-END
+END*/
