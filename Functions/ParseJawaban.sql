1277a77d5d61cd72d6b8683b4c9744f0f64cab35
edofarrell <edo.farrell17@gmail.com>
2023-06-02 10:31:58 +0700

    change function using openjson ParseJawaban.sql

--- Functions/ParseJawaban.sql
+++ Functions/ParseJawaban.sql
@@ -1,10 +1,81 @@
 /* 
+	Input Format (JSON Array):
+	[
+		{
+			"idPertanyaan": [NUMBER],
+			"jawaban":		[STRING]
+		},
+		{
+			"idPertanyaan": [NUMBER],
+			"jawaban":		[STRING]
+		}
+	]
+
+	Output Format:
+	key				value
+	idPertanyaan	[INT]
+	jawaban			[VARCHAR](150)
+*/
+ALTER FUNCTION [ParseJawaban] 
+(
+	@str [NVARCHAR](1000)
+)
+RETURNS @result TABLE
+(
+	[idPertanyaan] [INT],
+	[jawaban] [VARCHAR](150)
+)
+AS
+BEGIN
+	DECLARE 
+		@currJawaban [VARCHAR](150)
+
+	DECLARE cursorJawaban CURSOR
+	FOR
+		SELECT 
+			[value]
+		FROM
+			OPENJSON(@str)
+	OPEN cursorJawaban
+
+	FETCH NEXT FROM
+		cursorJawaban
+	INTO
+		@currJawaban
+
+	WHILE(@@FETCH_STATUS=0)
+	BEGIN
+		INSERT INTO @result
+		SELECT
+			[idPertanyaan],
+			[jawaban]
+		FROM
+			OPENJSON(@currJawaban)
+		WITH
+			(
+				[idPertanyaan] [INT] '$.idPertanyaan',
+				[jawaban] [VARCHAR](150) '$.jawaban'
+			)
+
+		FETCH NEXT FROM
+			cursorJawaban
+		INTO
+			@currJawaban
+		END
+	
+	CLOSE cursorJawaban
+	DEALLOCATE cursorJawaban
+
+	RETURN
+END
+
+/* 
 	String Format: "id1:jawaban1,id2:jawaban2,...,idn:jawabann"
 	First seperator: ','
 	Second seperator: ':'
 */
 
-ALTER FUNCTION [ParseJawaban] 
+/*ALTER FUNCTION [ParseJawaban] 
 (
 	@str [VARCHAR](1000)
 )
@@ -46,4 +117,4 @@ BEGIN
 	DEALLOCATE cursorRow
 
 	RETURN
-END
+END*/
