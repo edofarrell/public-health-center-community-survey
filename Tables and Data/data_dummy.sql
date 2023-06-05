1d64227c5cfffd4aa4ca05c7ca16c4661ef5bd2e
DearenHippy <6182001036@student.unpar.ac.id>
2023-06-05 16:51:52 +0700

    Fix data dummy pakai SP

--- Tables and Data/data_dummy.sql
+++ Tables and Data/data_dummy.sql
@@ -5,30 +5,123 @@ VALUES
 	('Kader'),
 	('Penanggung Jawab')
 
---Insert User
+--Create User
 --Dearen = Admin
 --Edo = Kader
 --Neil = Penanggung Jawab
-INSERT INTO
-	[User]([username], [password], [idRole])
-VALUES
-	('dearen', 'password', '1'),
-	('edo', 'password', '2'),
-	('neil', 'password', '3')
+EXEC CreateUser 'dearen','password',1
+EXEC CreateUser 'edo','password',2
+EXEC CreateUser 'neil','password',3
 
-INSERT INTO
-	[Survei]([namaSurvei], [timestamp], [tombstone])
-VALUES
-	('Covid19', CURRENT_TIMESTAMP, 1),
-	('Demam Berdarah', CURRENT_TIMESTAMP, 1),
-	('Diare', CURRENT_TIMESTAMP, 1)
+--Create Survei
+EXEC CreateSurvei 'Covid19'
+EXEC CreateSurvei 'Demam Berdarah'
+EXEC CreateSurvei 'Diare'
 
+--Create Akses ke Survei
 INSERT INTO
 	[MengaksesSurvei]([idUser], [idSurvei])
 VALUES
-	(2, 1),
+	(1, 1),
 	(2, 2),
-	(2, 3)
+	(3, 3)
+
+--Insert Jawaban Survei
+EXEC InsertJawaban 1,1,'
+[
+	{
+		"idPertanyaan": 1,
+		"jawaban": "Edo Farrell"
+	},
+	{
+		"idPertanyaan": 2,
+		"jawaban": 20
+	},
+	{
+		"idPertanyaan": 3,
+		"jawaban": "Sudah"
+	},
+	{
+		"idPertanyaan": 4,
+		"jawaban": "2023-01-20"
+	},
+	{
+		"idPertanyaan": 5,
+		"jawaban": "Pernah"
+	}
+]'
+
+EXEC InsertJawaban 1,1,'
+[
+	{
+		"idPertanyaan": 1,
+		"jawaban": "Erwin Darsono"
+	},
+	{
+		"idPertanyaan": 2,
+		"jawaban": 45
+	},
+	{
+		"idPertanyaan": 3,
+		"jawaban": "Sudah"
+	},
+	{
+		"idPertanyaan": 4,
+		"jawaban": "2018-05-23"
+	},
+	{
+		"idPertanyaan": 5,
+		"jawaban": "Belum"
+	}
+]'
+
+EXEC InsertJawaban 2,2,'
+[
+	{
+		"idPertanyaan": 6,
+		"jawaban": "Dearen Hippy"
+	},
+	{
+		"idPertanyaan": 7,
+		"jawaban": 19
+	},
+	{
+		"idPertanyaan": 8,
+		"jawaban": "2022-10-18"
+	},
+	{
+		"idPertanyaan": 9,
+		"jawaban": "Iyaa"
+	},
+	{
+		"idPertanyaan": 10,
+		"jawaban": 1
+	}
+]'
+
+EXEC InsertJawaban 3,3,'
+[
+	{
+		"idPertanyaan": 11,
+		"jawaban": "Neil Christopher"
+	},
+	{
+		"idPertanyaan": 12,
+		"jawaban": 19
+	},
+	{
+		"idPertanyaan": 13,
+		"jawaban": "2012-11-05"
+	},
+	{
+		"idPertanyaan": 14,
+		"jawaban": "Sering"
+	},
+	{
+		"idPertanyaan": 15,
+		"jawaban": "Punya"
+	}
+]'
 
 INSERT INTO
 	[PertanyaanSurvei]([pertanyaan], [tipeJawaban], [timestamp], [tombstone], [idSurvei])
