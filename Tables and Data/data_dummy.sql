c7e8706bf6dd63d610076f5c7ccad11224a8aa9d
edofarrell <edo.farrell17@gmail.com>
2023-06-05 17:16:49 +0700

    change insert into MengaksesSurvei table using sp

--- Tables and Data/data_dummy.sql
+++ Tables and Data/data_dummy.sql
@@ -19,118 +19,9 @@ EXEC CreateSurvei 'Demam Berdarah'
 EXEC CreateSurvei 'Diare'
 
 --Create Akses ke Survei
-INSERT INTO
-	[MengaksesSurvei]([idUser], [idSurvei])
-VALUES
-	(1, 1),
-	(2, 2),
-	(3, 3)
-
---Insert Jawaban Survei
-DECLARE @jawaban11 [NVARCHAR](3350) =
-'[
-	{
-		"idPertanyaan": 1,
-		"jawaban": "Edo Farrell"
-	},
-	{
-		"idPertanyaan": 2,
-		"jawaban": 20
-	},
-	{
-		"idPertanyaan": 3,
-		"jawaban": "Sudah"
-	},
-	{
-		"idPertanyaan": 4,
-		"jawaban": "2023-01-20"
-	},
-	{
-		"idPertanyaan": 5,
-		"jawaban": "Pernah"
-	}
-]'
-
-DECLARE @jawaban12 [NVARCHAR](3350) =
-'[
-	{
-		"idPertanyaan": 1,
-		"jawaban": "Erwin Darsono"
-	},
-	{
-		"idPertanyaan": 2,
-		"jawaban": 45
-	},
-	{
-		"idPertanyaan": 3,
-		"jawaban": "Sudah"
-	},
-	{
-		"idPertanyaan": 4,
-		"jawaban": "2018-05-23"
-	},
-	{
-		"idPertanyaan": 5,
-		"jawaban": "Belum"
-	},
-	{
-		"idPertanyaan": 6,
-		"jawaban": "Ada"
-	}
-]'
-
-DECLARE @jawaban21 [NVARCHAR](3350) =
-'[
-	{
-		"idPertanyaan": 6,
-		"jawaban": "Dearen Hippy"
-	},
-	{
-		"idPertanyaan": 7,
-		"jawaban": 19
-	},
-	{
-		"idPertanyaan": 8,
-		"jawaban": "2022-10-18"
-	},
-	{
-		"idPertanyaan": 9,
-		"jawaban": "Iyaa"
-	},
-	{
-		"idPertanyaan": 10,
-		"jawaban": 1
-	}
-]'
-
-DECLARE @jawaban31 [NVARCHAR](3350) =
-'[
-	{
-		"idPertanyaan": 11,
-		"jawaban": "Neil Christopher"
-	},
-	{
-		"idPertanyaan": 12,
-		"jawaban": 19
-	},
-	{
-		"idPertanyaan": 13,
-		"jawaban": "2012-11-05"
-	},
-	{
-		"idPertanyaan": 14,
-		"jawaban": "Sering"
-	},
-	{
-		"idPertanyaan": 15,
-		"jawaban": "Punya"
-	}
-]'
-
-EXEC InsertJawaban 1, 1, @jawaban11
-EXEC InsertJawaban 1, 1, @jawaban12
-EXEC InsertJawaban 2, 2, @jawaban21
-EXEC InsertJawaban 3, 3, @jawaban31
+EXEC CreateAkses 2, 1
+EXEC CreateAkses 2, 2
+EXEC CreateAkses 2, 3
 
 DECLARE @pertanyaanSurvei1 [NVARCHAR](2150) = 
 '[
@@ -215,3 +106,109 @@ DECLARE @pertanyaanSurvei3 [NVARCHAR](2150) =
 EXEC InsertPertanyaan 1, @pertanyaanSurvei1
 EXEC InsertPertanyaan 2, @pertanyaanSurvei2
 EXEC InsertPertanyaan 3, @pertanyaanSurvei3
+
+--Insert Jawaban Survei
+DECLARE @jawaban11 [NVARCHAR](3350) =
+'[
+	{
+		"idPertanyaan": 1,
+		"jawaban": "Edo Farrell"
+	},
+	{
+		"idPertanyaan": 2,
+		"jawaban": 20
+	},
+	{
+		"idPertanyaan": 3,
+		"jawaban": "Sudah"
+	},
+	{
+		"idPertanyaan": 4,
+		"jawaban": "2023-01-20"
+	},
+	{
+		"idPertanyaan": 5,
+		"jawaban": "Pernah"
+	}
+]'
+
+DECLARE @jawaban12 [NVARCHAR](3350) =
+'[
+	{
+		"idPertanyaan": 1,
+		"jawaban": "Erwin Darsono"
+	},
+	{
+		"idPertanyaan": 2,
+		"jawaban": 45
+	},
+	{
+		"idPertanyaan": 3,
+		"jawaban": "Sudah"
+	},
+	{
+		"idPertanyaan": 4,
+		"jawaban": "2018-05-23"
+	},
+	{
+		"idPertanyaan": 5,
+		"jawaban": "Belum"
+	},
+	{
+		"idPertanyaan": 6,
+		"jawaban": "Ada"
+	}
+]'
+
+DECLARE @jawaban21 [NVARCHAR](3350) =
+'[
+	{
+		"idPertanyaan": 8,
+		"jawaban": "Dearen Hippy"
+	},
+	{
+		"idPertanyaan": 9,
+		"jawaban": 19
+	},
+	{
+		"idPertanyaan": 10,
+		"jawaban": "2022-10-18"
+	},
+	{
+		"idPertanyaan": 11,
+		"jawaban": "Iyaa"
+	},
+	{
+		"idPertanyaan": 12,
+		"jawaban": 1
+	}
+]'
+
+DECLARE @jawaban31 [NVARCHAR](3350) =
+'[
+	{
+		"idPertanyaan": 13,
+		"jawaban": "Neil Christopher"
+	},
+	{
+		"idPertanyaan": 14,
+		"jawaban": 19
+	},
+	{
+		"idPertanyaan": 15,
+		"jawaban": "2012-11-05"
+	},
+	{
+		"idPertanyaan": 16,
+		"jawaban": "Sering"
+	},
+	{
+		"idPertanyaan": 17,
+		"jawaban": "Punya"
+	}
+]'
+
+EXEC InsertJawaban 2, 1, @jawaban11
+EXEC InsertJawaban 2, 1, @jawaban12
+EXEC InsertJawaban 2, 2, @jawaban21
+EXEC InsertJawaban 2, 3, @jawaban31
