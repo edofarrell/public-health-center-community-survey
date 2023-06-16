3b11756b6ed701351f21ab4ec768db7783cd0c76
edofarrell <edo.farrell17@gmail.com>
2023-06-16 21:54:06 +0700

    edit sp GetGroupJawaban.sql

--- Stored Procedures/GetGroupJawaban.sql
+++ Stored Procedures/GetGroupJawaban.sql
@@ -1,3 +1,7 @@
+/*
+	@waktuAwal dan @waktuakhir dapat keduanya bernilai null atau keduanya terisi,
+	tidak bisa hanya salah satu
+*/
 ALTER PROCEDURE [GetGroupJawaban]
 	@idSurvei [INT],
 	@waktuAwal [DATETIME],
@@ -5,16 +9,30 @@ ALTER PROCEDURE [GetGroupJawaban]
 AS
 	BEGIN TRANSACTION
 	BEGIN TRY
-		SELECT
-			[idGroupJawaban]
-		FROM
-			[GroupJawaban]
-		WHERE
-			[idSurvei] = @idSurvei
-			AND [timestamp] >= @waktuAwal
-			AND [timestamp] <= @waktuAkhir
-		ORDER BY
-			[idGroupJawaban] ASC
+		IF(@waktuAwal IS NULL OR @waktuAkhir IS NULL)
+		BEGIN
+			SELECT
+				[idGroupJawaban]
+			FROM
+				[GroupJawaban]
+			WHERE
+				[idSurvei] = @idSurvei
+			ORDER BY
+				[idGroupJawaban] ASC
+		END
+		ELSE
+		BEGIN
+			SELECT
+				[idGroupJawaban]
+			FROM
+				[GroupJawaban]
+			WHERE
+				[idSurvei] = @idSurvei
+				AND [timestamp] >= @waktuAwal
+				AND [timestamp] <= @waktuAkhir
+			ORDER BY
+				[idGroupJawaban] ASC
+		END
 
 		COMMIT TRANSACTION
 	END TRY
