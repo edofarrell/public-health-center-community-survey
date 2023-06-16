91f750a1d3e8e1f74990035c14951854b4a19239
DearenHippy <6182001036@student.unpar.ac.id>
2023-06-16 22:01:06 +0700

    Create SP GetNamaSurvei and GetUser

---
+++ Stored Procedures/GetNamaSurvei.sql
@@ -0,0 +1,18 @@
+ALTER PROCEDURE [GetNamaSurvei]
+	@idSurvei [INT]
+AS
+	BEGIN TRANSACTION
+	BEGIN TRY
+		SELECT
+			[Survei].[namaSurvei]
+		FROM
+			[Survei]
+		WHERE
+			[Survei].[idSurvei] = @idSurvei
+		COMMIT TRANSACTION
+	END TRY
+	BEGIN CATCH
+		SELECT
+			0
+		ROLLBACK TRANSACTION
+	END CATCH
---
+++ Stored Procedures/GetUser.sql
@@ -0,0 +1,22 @@
+ALTER PROCEDURE [GetUser]
+AS
+	BEGIN TRANSACTION
+	BEGIN TRY
+		SELECT
+			[User].[username],
+			[User].[password],
+			[Role].[namaRole]
+		FROM
+			[User]
+			INNER JOIN [Role]
+		ON
+			[User].[idRole] = [Role].[idRole]
+		COMMIT TRANSACTION
+	END TRY
+	BEGIN CATCH
+		SELECT
+			0
+		ROLLBACK TRANSACTION
+	END CATCH
+
+EXEC GetUser
