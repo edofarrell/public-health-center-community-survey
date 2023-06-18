af1fbfb601cce5b8652d588f6f2cf21a988d2daa
edofarrell <edo.farrell17@gmail.com>
2023-06-18 11:20:49 +0700

    add dummy data

--- Tables and Data/data_dummy.sql
+++ Tables and Data/data_dummy.sql
@@ -109,7 +109,7 @@ DECLARE @jawaban11 [NVARCHAR](3350) =
 '[
 	{
 		"idPertanyaan": 1,
-		"jawaban": "Edo Farrell"
+		"jawaban": "Edo"
 	},
 	{
 		"idPertanyaan": 2,
@@ -209,3 +209,26 @@ EXEC InsertJawaban 2, 1, @jawaban11
 EXEC InsertJawaban 2, 1, @jawaban12
 EXEC InsertJawaban 2, 2, @jawaban21
 EXEC InsertJawaban 2, 3, @jawaban31
+
+DECLARE @jawabanEdit11 [NVARCHAR](3350) =
+'[
+	{
+		"idPertanyaan": 1,
+		"jawaban": "Edo F"
+	},
+	{
+		"idPertanyaan": 2,
+		"jawaban": 21
+	}
+]'
+
+DECLARE @jawabanEdit12 [NVARCHAR](3350) =
+'[
+	{
+		"idPertanyaan": 1,
+		"jawaban": "Edo Farrell"
+	}
+]'
+
+EXEC UpdateJawaban 2, 1, @jawabanEdit11
+EXEC UpdateJawaban 2, 1, @jawabanEdit12
